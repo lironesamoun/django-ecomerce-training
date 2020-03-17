d699fdcf104c78d5065c1fb9821258b23fd39e61
lirone <lirone.samoun@gmail.com>
2020-03-17 12:28:06 +0100

    init project ecommerce

--- core/models.py
+++ core/models.py
@@ -1,15 +1,33 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 
 
 class Item(models.Model):
-    pass
+    title = models.CharField(max_length=100)
+    prince = models.FloatField()
+
+    def __str__(self):
+        return self.title
 
 # Link the item with the order itself
+
+
 class OrderItem(models.Model):
-    pass
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
 
 
 class Order(models.Model):
-    pass
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    items = models.ManyToManyField(OrderItem)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField()
+    order = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+
+
+def item_list(request):
+
+    context = {
+        'items': Item.objects.all(),
+    }
+    return render(request, "core/home-page.html", context)
